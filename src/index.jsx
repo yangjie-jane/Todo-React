af6c79833d7f8b1944f9ac1c9067235b8d86a8d2
Yang Jie <jiyang@thoughtworks.com>
2020-06-19 17:21:38 +0800

    todo basic function.

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoApp from './TodoApp';
+
+ReactDOM.render(<TodoApp/>, document.getElementById('root'));
